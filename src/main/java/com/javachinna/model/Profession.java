53bfc04efd0b1fdbc59b6ea9ae9313b71b10cd0b
farouk-hajjej <74997699+farouk-hajjej@users.noreply.github.com>
2022-03-18 23:22:00 +0100

    test3

--- src/main/java/com/javachinna/model/Profession.java
+++ src/main/java/com/javachinna/model/Profession.java
@@ -1,5 +1,11 @@
 package com.javachinna.model;
 
 public enum Profession {
-    FORMER,LEARNER,ADMIN,SPUSER
+    FORMER,LEARNER,ADMIN,SPUSER,
+    Gynecologist,
+    Cardiologist,
+    Dermatologist,
+    Psy,
+    OTHER
+
 }
--- src/main/java/com/javachinna/model/User.java
+++ src/main/java/com/javachinna/model/User.java
@@ -61,7 +61,7 @@ public class User implements Serializable {
 	// bi-directional many-to-many association to Role
 	@JsonIgnore
 	@ManyToMany
-//	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = { @JoinColumn(name = "ROLE_ID") })
+	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = { @JoinColumn(name = "ROLE_ID") })
 	private Set<Role> roles;
 
 
